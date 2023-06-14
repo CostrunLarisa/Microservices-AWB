c3d7e19328e7fe98a60b07baf25ccb6ff2c6fb49
Costrun Larisa <larisa-bianca.costrun@ing.com>
2023-06-15 01:46:03 +0300

    Fixed bean error.

--- client-service-2/src/main/java/com/unibuc/ro/ClientService2Application.java
+++ client-service-2/src/main/java/com/unibuc/ro/ClientService2Application.java
@@ -2,13 +2,14 @@ package com.unibuc.ro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 @EnableFeignClients
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ClientService2Application {

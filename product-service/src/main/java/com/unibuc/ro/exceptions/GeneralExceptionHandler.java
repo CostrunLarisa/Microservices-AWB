640969007dfc3bf10243f0d0b61679257afad909
Costrun Larisa <larisa-bianca.costrun@ing.com>
2023-06-17 14:54:09 +0300

    Added workaround for /products return 500.

--- product-service/src/main/java/com/unibuc/ro/exceptions/GeneralExceptionHandler.java
+++ product-service/src/main/java/com/unibuc/ro/exceptions/GeneralExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
@@ -15,5 +16,9 @@ public class GeneralExceptionHandler {
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<String> handle(EntityAlreadyExistsException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
+    public ResponseEntity<String> handle(HttpServerErrorException.InternalServerError e) {
+        return new ResponseEntity<>("A server error has occurred when processing the request.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

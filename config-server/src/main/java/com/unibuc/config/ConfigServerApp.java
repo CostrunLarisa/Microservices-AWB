79306862c3357668356eb6d50408c2488357236c
Costrun Larisa <larisa-bianca.costrun@ing.com>
2023-06-17 11:00:53 +0300

    import config properties working

--- config-server/src/main/java/com/unibuc/config/ConfigServerApp.java
+++ config-server/src/main/java/com/unibuc/config/ConfigServerApp.java
@@ -3,10 +3,12 @@ package com.unibuc.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
 @EnableConfigServer
+@PropertySource("classpath:application.properties")
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);

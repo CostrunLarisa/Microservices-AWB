0ee83de8bcee8355fc985cd17b4369b7c21e3bd8
Costrun Larisa <larisa-bianca.costrun@ing.com>
2023-06-18 15:47:44 +0300

    Added optional.

--- config-server/src/main/java/com/unibuc/config/ConfigServerApp.java
+++ config-server/src/main/java/com/unibuc/config/ConfigServerApp.java
@@ -3,13 +3,11 @@ package com.unibuc.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
 @EnableConfigServer
-@Profile("git")
 @PropertySource("classpath:application.properties")
 public class ConfigServerApp {
     public static void main(String[] args) {
